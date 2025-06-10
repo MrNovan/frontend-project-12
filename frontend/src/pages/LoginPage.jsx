c052963e978d40206ebe5233c42bac387018e7c5
MrNovan <toni.justdoit@bk.ru>
2025-06-10 09:43:23 +0300

    step 3

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,41 +1,96 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useFormik } from 'formik';
+import { Form, Button, Container, Alert } from 'react-bootstrap';
+import { userLogIn } from '../slices/authSlice.js';
+import { useDispatch } from 'react-redux'
+import { useNavigate, Link } from 'react-router-dom';
+import { useRollbar } from '@rollbar/react';
+import axios from 'axios';
+import { useTranslation } from 'react-i18next';
+
 
 const LoginPage = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const usernameInputRef = useRef(null);
+  const { t } = useTranslation();
+  const rollbar = useRollbar();
+
   const formik = useFormik({
     initialValues: {
       username: '',
       password: '',
     },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
+    onSubmit: async (values) => {
+      setError(null);
+      setDisabled(true);
+      try {
+        const response = await axios.post('/api/v1/login', values);
+        const { token, username } = response.data;
+        dispatch(userLogIn({ username, token }));
+        navigate('/');
+      } catch(error) {
+        if (error.code === 'ERR_NETWORK') {
+          setError(t('errors.network'));
+        } else if (error.code === 'ERR_BAD_REQUEST') {
+          setError(t('errors.incorrectPasswordOrUsername'));
+        } else {
+          rollbar.error('Ошибка при входе', error);
+          setError(t('errors.unknown'));
+        }
+      } finally {
+        setDisabled(false);
+      }
+    }
   });
-  console.log('Login page');
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="username">User Name</label>
-      <input
-        id="username"
-        name="username"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.username}
-      />
 
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        onChange={formik.handleChange}
-        value={formik.values.password}
-      />
-      <button type="submit">Submit</button>
-    </form>
+  useEffect(() => {
+    usernameInputRef.current.focus();
+  }, []);
+
+  return (
+    <>
+      <SimpleHeader />
+      <Container className="mt-5" style={{ maxWidth: '400px' }}>
+        <h1 className="text-center mb-4">{t('loginPage.login')}</h1>
+        <Form onSubmit={formik.handleSubmit}>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label>{t('loginPage.yourUsername')}</Form.Label>
+            <Form.Control
+              ref={usernameInputRef}
+              id="username"
+              type="text"
+              name="username"
+              placeholder={t('loginPage.username')}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.username}
+            />
+          </Form.Group>
+  
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>{t('password')}</Form.Label>
+            <Form.Control
+              id="password"
+              type="password"
+              name="password"
+              placeholder={t('password')}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+            />
+          </Form.Group>
+          {error && (<Alert variant="danger" className="mb-3">{error}</Alert>)}
+          <Button className="w-100" variant="primary" type="submit" disabled={disabled}>{t('loginPage.login')}</Button>
+          <Form.Text className="text-center mt-3 d-block">
+            {t('loginPage.noAcc')} <Link to="/signup">{t('loginPage.signupNavigate')}</Link>
+          </Form.Text>
+        </Form>
+      </Container>
+    </>
   );
-};
+}
 
 export default LoginPage;
---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: {
+    username: localStorage.getItem('username') || null,
+    token: localStorage.getItem('token') || null,
+  }
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    userLogIn: (state, { payload }) => {
+      const { username, token } = payload;
+      state.user = { username, token };
+      localStorage.setItem('username', username);
+      localStorage.setItem('token', token);
+    },
+    userLogOut: (state) => {
+      state.user.username = null;
+      state.user.token = null;
+      localStorage.removeItem('username');
+      localStorage.removeItem('token');
+    }
+  }
+});
+
+export const { userLogIn, userLogOut } = authSlice.actions;
+export default authSlice.reducer;
